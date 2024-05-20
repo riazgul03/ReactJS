4c9c81359df6cf7f2e0ce1c53e07e7f457a060b4
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-05-20 08:39:08 -0700

    Constructor in React

---
+++ MYJS/MyScript.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(name,age){
+
+        this.name = name ;
+        this.age = age ;
+        //console.log("Hello I am Constructors");
+    }
+}
+const std = new Student("Adil", 22);
+console.log(std.name);
+console.log(std.age);
+
+const std1 = new Student("Kumar", 30);
+console.log(std1.name);
+console.log(std1.age);
