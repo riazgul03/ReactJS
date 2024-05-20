dcf3ecc06ce56f333f2be5d327215c6e58c218a7
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-05-20 08:50:12 -0700

    Constructors

--- MYJS/MyScript.js
+++ MYJS/MyScript.js
@@ -10,6 +10,6 @@ const std = new Student("Adil", 22);
 console.log(std.name);
 console.log(std.age);
 
-const std1 = new Student("Kumar", 30);
+const std1 = new Student("Khan", 30);
 console.log(std1.name);
 console.log(std1.age);
