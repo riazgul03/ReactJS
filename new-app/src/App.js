5209fdcf1336fd54973eef263f8f7b8a9c7428d4
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-19 09:01:58 -0700

    Props With The Class Component in React

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -5,7 +5,9 @@ import React,{Component} from 'react';
 export class App extends React.Component{
   render(){
     return(
-      <h1>Muhammad Adil</h1>
+     <>
+      
+     </>
     )
   }
 }
--- new-app/src/index.js
+++ new-app/src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-import User from './User';
-import HelloMessage from './HelloMessage';
-import House from './House';
-
+import MyClass from './MyClass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let a = "Adil";
 root.render(
-<React.StrictMode>
- <App />
- {/* <App2/> */}
+  <React.StrictMode>
+    <MyClass name={a} age={22}>
+<h2>Child No : 1</h2>
+    </MyClass>
+    <MyClass name="Kumar" age={26}>
+    <button type='button' >Submit</button>
+    </MyClass>
+    <MyClass name="Amit" age={28}>
+    <ol>
+      <li>Ali</li>
+      <li>Zain</li>
+      <li>Noman</li>
+    </ol>
+    </MyClass>
   </React.StrictMode>
 );
 
