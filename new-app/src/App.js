1475796c3b624d8ee7936a132949bc6a9b97b585
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-21 09:41:31 -0700

    UseState Hook In React

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import CSSstylesheet from './CSSstylesheet';
 import Stylesheet1 from './Stylesheet1';
 import Stylesheet2 from './Stylesheet2';
+import USComponent from './USComponent';
 
 
 
 function App(){
   return(
     <div>
-    <Stylesheet1/>
-    <Stylesheet2/>
+   <USComponent/>
     </div>
   )
 }
