bf1df8ffbb4d0725d55ef87d3b4c1ca9e562fe5e
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-21 09:58:45 -0700

    UseState Hook With Object In React

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -4,13 +4,14 @@ import CSSstylesheet from './CSSstylesheet';
 import Stylesheet1 from './Stylesheet1';
 import Stylesheet2 from './Stylesheet2';
 import USComponent from './USComponent';
+import USWithObject from './USWithObject';
 
 
 
 function App(){
   return(
     <div>
-   <USComponent/>
+   <USWithObject/>
     </div>
   )
 }
