7453e1307e2fbd0ef2e0f817983c893bf387a0ce
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-21 05:18:51 -0700

    Styling React Component-CSS Module In React

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import CSSstylesheet from './CSSstylesheet';
+import Stylesheet1 from './Stylesheet1';
+import Stylesheet2 from './Stylesheet2';
 
 
 
 function App(){
   return(
     <div>
-    <CSSstylesheet check={false}/>
+    <Stylesheet1/>
+    <Stylesheet2/>
     </div>
   )
 }
