5f3902539255c27d25b2cb1feadd10e26d5effd3
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-22 08:49:23 -0700

    UseEffect Hook In React

--- new-app/src/FunctionEffect.js
+++ new-app/src/FunctionEffect.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react'
 
 function FunctionEffect() {
-  let [count, setCount]= useState( 0 )
-     
-     useEffect( () =>{
+    let [count, setCount] = useState(0)
+
+    useEffect(() => {
         console.log("Hello I am Effect = " + count)
-     })
-  return (
-    <div>
-    <h1>{count}</h1>
-    <button onClick={() => setCount(count + 1)}>Update State</button>
-    </div>
-  )
+    })
+    return (
+        <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Update State</button>
+        </div>
+    )
 }
 
-export default FunctionEffect
+export default FunctionEffect 
