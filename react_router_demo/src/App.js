7986967ba344897881651039042473ad81c261c4
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-28 11:08:40 -0700

    Navigating Programmatically In React

--- react_router_demo/src/App.js
+++ react_router_demo/src/App.js
@@ -1,19 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Navbar from './Components/Navbar';
 function App() {
+  const navigate = useNavigate();
+  // const NavigateToAbout = () =>{
+  //   navigate('/about');
+  // }
+
+
+
+  const NavigateToWhere = () => {
+    let name = 'Kumar'
+    if (name == 'Adil') {
+      navigate('/about')
+    } else {
+      navigate('/contact')
+    }
+  }
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/About' element={<About/>}/>
-      <Route path='/Contact' element={<Contact/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
+      {/* <button onClick={() => navigate('/about')}>About</button>
+      <button onClick={() => NavigateToAbout()}>About</button>
+      <button onClick={() => navigate('/contact')}>Contact</button>
+      <button onClick={() => NavigateTo('/about')}>About</button>
+      <button onClick={() => NavigateTo('/contact')}>Contact</button> */}
+      <button onClick={() => NavigateToWhere()}>Click Me</button>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+
     </>
   );
 }
--- react_router_demo/src/Components/Contact.js
+++ react_router_demo/src/Components/Contact.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Contact() {
+  const navigate = useNavigate();
   return (
     <div>
-        <h1>Welcome To My  Contact Page</h1>
+      <h1>Welcome To My  Contact Page</h1>
+      {/* <button onClick={() => navigate(-1)}>Go Back</button> */}
     </div>
   )
 }
