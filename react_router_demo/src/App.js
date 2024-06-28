e989115607d2008c090fab018b67e563a1648220
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-28 09:44:28 -0700

    Link Component Router In React

--- react_router_demo/src/App.js
+++ react_router_demo/src/App.js
@@ -4,14 +4,17 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import Navbar from './Components/Navbar';
 function App() {
   return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/About' element={<About/>}/>
       <Route path='/Contact' element={<Contact/>}/>
-
     </Routes>
+    </>
   );
 }
 
