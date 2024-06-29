f312f342c1773dd7cc7354ff67756b974f3e3df5
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-29 10:05:35 -0700

    UseFarams In React Router

--- react_router_demo/src/Components/UsersDetails.js
+++ react_router_demo/src/Components/UsersDetails.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function UsersDetails() {
+  // const UserId = useParams();
+  // const id = UserId.id;
+  const {id} = useParams();
   return (
     <div>
-        <h2>Users Details Page..</h2>
+        <h2>Users Details Page -- {id}</h2>
     </div>
   )
 }
