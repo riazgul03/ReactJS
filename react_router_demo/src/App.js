e797968802d88e01096f2565de6bc606d65a8924
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-06-29 03:28:03 -0700

    Nested Routes In React Router

--- react_router_demo/src/App.js
+++ react_router_demo/src/App.js
@@ -6,16 +6,22 @@ import About from './Components/About';
 import Contact from './Components/Contact';
 import Navbar from './Components/Navbar';
 import PageNotFound from './Components/PageNotFound';
+import Products from './Components/Products';
+import Shirts from './Components/Shirts';
+import Jeans from './Components/Jeans';
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Contact' element={<Contact />} />
+        <Route path='/products' element={<Products/>}>
+          <Route path='shirts' element={<Shirts/>}/>
+          <Route path='jeans' element={<Jeans/>}/>
+        </Route>
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='*' element={<PageNotFound />} />
-
       </Routes>
 
 
