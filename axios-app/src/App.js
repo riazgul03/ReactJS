a6c7f330a90674d6e7e2a302dc3790daf8405793
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-07-19 10:17:18 -0700

    Post Request In Axios In React

--- axios-app/src/App.js
+++ axios-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import GetAxios from './GetAxios';
+import PostAxios from './PostAxios';
 
 function App() {
   return (
     <div className="App">
-      <GetAxios/>
+      <PostAxios/>
     </div>
   );
 }
