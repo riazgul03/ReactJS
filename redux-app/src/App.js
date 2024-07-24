ef53905b4b24de0619a00ab8f70d6a6661dfd98f
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-07-24 04:55:28 -0700

    Actions In React-Redux

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To React Redux</h1>
+      <input type="text" />
+      <br />
+      <button className='btn btn-primary'>Add</button>
+      <button className='btn btn-danger'>Minus</button>
     </div>
   );
 }
