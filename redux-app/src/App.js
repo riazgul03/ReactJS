69b37298c609ab46002249e6e340a6ff6cbb673f
riazgul03 <168696204+riazgul03@users.noreply.github.com>
2024-07-25 04:32:57 -0700

    Final Result Of Store & Actions & Reducers Example In Redux

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increase, decrease } from './actions'
 
 function App() {
+  const myState = useSelector((state) => state.inc_dec);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Welcome To React Redux</h1>
-      <input type="text" />
+      <input type="text" value={myState} />
       <br />
-      <button className='btn btn-primary'>Add</button>
-      <button className='btn btn-danger'>Minus</button>
+      <button onClick={() => dispatch(increase())} className='btn btn-primary'>Add</button>
+      <button onClick={() => dispatch(decrease())} className='btn btn-danger'>Minus</button>
     </div>
   );
 }
--- redux-app/src/reducers/inc_dec.js
+++ redux-app/src/reducers/inc_dec.js
@@ -1,11 +1,11 @@
 const initialState = 0;
 
-const inc_dec = (initialState, action) => {
+const inc_dec = (state = initialState, action) => {
     switch (action.type) {
         case "INCREMENT":
-            return state + 1
+            return state + 1;
         case "DECREMENT":
-            return state - 1
+            return state - 1;
         default: return state;
     }
 }
